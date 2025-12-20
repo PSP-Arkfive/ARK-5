9c0ff6946596af0dfb251a3551fb7696f53b9ab8
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2025-12-20 02:48:43 +0100

    cleanup

--- Core/Compat/PSP/rebootex/main.c
+++ Core/Compat/PSP/rebootex/main.c
@@ -16,10 +16,10 @@ int cfwBoot(int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7
     // Configure
     bootConfig(FLASH_BOOT, TYPE_REBOOTEX, NULL);
 
-    // scan for reboot functions
+    // scan functions
     findBootFunctions();
     
-    // patch reboot buffer
+    // patch sceboot
     patchBootPSP();
 
     // Forward Call
--- Core/Compat/ePSP/rebootex/main.c
+++ Core/Compat/ePSP/rebootex/main.c
@@ -22,10 +22,10 @@ int cfwBoot(int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7
     // Configure
     bootConfig(FLASH_BOOT, TYPE_REBOOTEX, &iofuncs);
 
-    // scan for reboot functions
+    // scan functions
     findBootFunctions();
     
-    // patch reboot buffer
+    // patch sceboot
     patchBootVita();
     
     // Forward Call
