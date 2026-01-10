424a5853346b9c20e1b42f00c13fd288cc6613c5
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2026-01-10 18:05:19 +0100

    improve extended utility

--- Core/SystemControl/src/utility.c
+++ Core/SystemControl/src/utility.c
@@ -9,18 +9,8 @@
 #include <cfwmacros.h>
 
 
-enum{ // internal id
-    MODULE_NET_FTP,
-    MODULE_AV_PNG,
-    MODULE_AV_PLAYER,
-    MODULE_VLF,
-    MODULE_INTRAFONT_VLF,
-    MODULE_INTRAFONT_GU,
-    MODULE_UNARCHIVER,
-    N_MODULES
-};
-
 typedef struct {
+    int id;
     int modid;
     char* prxname;
     char* libname;
@@ -29,21 +19,30 @@ typedef struct {
 
 extern ARKConfig* ark_config;
 
-static CustomUtilityModule custom_utility_modules[N_MODULES] = {
-    {-1, PSPFTP_PRX, NULL},
-    {-1, LIBPNG_PRX, NULL},
-    {-1, PSPAV_PRX, NULL},
-    {-1, VLF_PRX, NULL},
-    {-1, "VLFFONT.PRX", "intraFont-vlf.prx"},
-    {-1, INTRAFONT_PRX, "intraFont-gu.prx"},
-    {-1, UNARCHIVE_PRX, "unarchiver.prx"},
+static CustomUtilityModule custom_utility_modules[] = {
+    {PSP_MODULE_NET_FTP, -1, PSPFTP_PRX, NULL},
+    {PSP_MODULE_AV_PNG, -1, LIBPNG_PRX, NULL},
+    {PSP_MODULE_AV_PLAYER, -1, PSPAV_PRX, NULL},
+    {PSP_MODULE_VLF, -1, VLF_PRX, NULL},
+    {PSP_MODULE_INTRAFONT_VLF, -1, "VLFFONT.PRX", "intraFont-vlf.prx"},
+    {PSP_MODULE_INTRAFONT_GU, -1, INTRAFONT_PRX, "intraFont-gu.prx"},
+    {PSP_MODULE_UNARCHIVER, -1, UNARCHIVE_PRX, "unarchiver.prx"},
 };
 
 static int (*origUtilityLoadModule)(int);
 static int (*origUtilityUnloadModule)(int);
 
-static void findModulePath(CustomUtilityModule* module, char* path){
+static CustomUtilityModule* findCustomModuleById(int id){
+    if (module>=0x0700 || (module&0xFF) >= 0x80){ // custom module
+        for (int i=0; i<NELEMS(custom_utility_modules); i++){
+            CustomUtilityModule* module = &custom_utility_modules[i];
+            if (module->id == id) return module;
+        }
+    }
+    return NULL;
+}
 
+static void findModulePath(CustomUtilityModule* module, char* path){
     SceIoStat stat;
     char* libname = module->libname? module->libname : module->prxname;
 
@@ -68,14 +67,17 @@ static void findModulePath(CustomUtilityModule* module, char* path){
     // try in ark path
     strcpy(path, ark_config->arkpath);
     strcat(path, module->prxname);
+
+    if (sceIoGetstat(path, &stat) >= 0) return; // found
+
+    // try in flash0
+    strcpy(path, module->f0path);
 }
 
-static int loadstartCustomUtilityModule(int internal_id){
-    CustomUtilityModule* module = &custom_utility_modules[internal_id];
+static int loadstartCustomUtilityModule(CustomUtilityModule* module){
     if (module->modid >= 0) return 0x80111102; // SCE_UTILITY_MODULE_ERROR_ALREADY_LOADED
 
     char modpath[ARK_PATH_SIZE];
-
     findModulePath(module, modpath);    
 
     module->modid = sceKernelLoadModule(modpath, 0, NULL);
@@ -90,12 +92,10 @@ static int loadstartCustomUtilityModule(int internal_id){
     return 0;
 }
 
-static int stopunloadCustomUtilityModule(int internal_id){
-    CustomUtilityModule* module = &custom_utility_modules[internal_id];
+static int stopunloadCustomUtilityModule(CustomUtilityModule* module){
     if (module->modid < 0) return 0x80111103; // SCE_UTILITY_MODULE_ERROR_NOT_LOADED
 
     int res = sceKernelStopModule(module->modid, 0, NULL, NULL, NULL);
-
     if (res >= 0){
         res = sceKernelUnloadModule(module->modid);
         if (res >= 0){
@@ -109,32 +109,11 @@ static int stopunloadCustomUtilityModule(int internal_id){
 static int extendedUtilityLoadModule(int module){
     int k1 = pspSdkSetK1(0);
     int res = -1;
-    switch (module){
-        case PSP_MODULE_NET_FTP:
-            res = loadstartCustomUtilityModule(MODULE_NET_FTP);
-            break;
-        case PSP_MODULE_AV_PNG:
-            res = loadstartCustomUtilityModule(MODULE_AV_PNG);
-            break;
-        case PSP_MODULE_AV_PLAYER:
-            res = loadstartCustomUtilityModule(MODULE_AV_PLAYER);
-            break;
-        case PSP_MODULE_VLF:
-            res = loadstartCustomUtilityModule(MODULE_VLF);
-            break;
-        case PSP_MODULE_INTRAFONT_VLF:
-            res = loadstartCustomUtilityModule(MODULE_INTRAFONT_VLF);
-            break;
-        case PSP_MODULE_INTRAFONT_GU:
-            res = loadstartCustomUtilityModule(MODULE_INTRAFONT_GU);
-            break;
-        case PSP_MODULE_UNARCHIVER:
-            res = loadstartCustomUtilityModule(MODULE_UNARCHIVER);
-            break;
-        default:
-            res = origUtilityLoadModule(module);
-            break;
-    }
+
+    CustomUtilityModule* mod = findCustomModuleById(module);
+    if (mod) res = loadstartCustomUtilityModule(mod);
+    else res = origUtilityLoadModule(module);
+
     pspSdkSetK1(k1);
     return res;
 }
@@ -142,32 +121,11 @@ static int extendedUtilityLoadModule(int module){
 static int extendedUtilityUnloadModule(int module){
     int k1 = pspSdkSetK1(0);
     int res = -1;
-    switch (module){
-        case PSP_MODULE_NET_FTP:
-            res = stopunloadCustomUtilityModule(MODULE_NET_FTP);
-            break;
-        case PSP_MODULE_AV_PNG:
-            res = stopunloadCustomUtilityModule(MODULE_AV_PNG);
-            break;
-        case PSP_MODULE_AV_PLAYER:
-            res = stopunloadCustomUtilityModule(MODULE_AV_PLAYER);
-            break;
-        case PSP_MODULE_VLF:
-            res = stopunloadCustomUtilityModule(MODULE_VLF);
-            break;
-        case PSP_MODULE_INTRAFONT_VLF:
-            res = stopunloadCustomUtilityModule(MODULE_INTRAFONT_VLF);
-            break;
-        case PSP_MODULE_INTRAFONT_GU:
-            res = stopunloadCustomUtilityModule(MODULE_INTRAFONT_GU);
-            break;
-        case PSP_MODULE_UNARCHIVER:
-            res = stopunloadCustomUtilityModule(MODULE_UNARCHIVER);
-            break;
-        default:
-            res = origUtilityUnloadModule(module);
-            break;
-    }
+    
+    CustomUtilityModule* mod = findCustomModuleById(module);
+    if (mod) res = stopunloadCustomUtilityModule(mod);
+    else res = origUtilityUnloadModule(module);
+
     pspSdkSetK1(k1);
     return res;
 }
