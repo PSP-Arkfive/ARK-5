1a632bc6e045c0cfce6123d9fcdda7e1d30fe9c9
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2026-01-16 20:46:33 +0100

    allow per-cfw plugins

--- Core/SystemControl/src/plugin.c
+++ Core/SystemControl/src/plugin.c
@@ -187,6 +187,13 @@ static int matchingRunlevel(char * runlevel)
     
     lowerString(runlevel, runlevel, strlen(runlevel)+1);
 
+    char* cfw_type = strstr(runlevel, "cfw=");
+    if (cfw_type){ // plugin is for specific CFW only
+        if (strncmp(cfw_type+4, "ark", 3) != 0){
+            return 0; // not for ark, treat as disabled
+        }
+    }
+
     if (strcasecmp(runlevel, "all") == 0 || strcasecmp(runlevel, "always") == 0) return 1; // always on
 
     if (strchr(runlevel, '/')){
